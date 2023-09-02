259f5eedce6152767e2c876fa4a22df173c21d55
Tamil8248 <143870207+Tamil8248@users.noreply.github.com>
2023-09-02 13:24:56 +0530

    Add files via upload

---
+++ Opengoogle.java
@@ -0,0 +1,15 @@
+package org.GoogleOpen.com;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Opengoogle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.setProperty("webDriver.chrome.driver", "C:\\Users\\Admin\\Desktop\\selenuim\\chromedriver.exe");
+		WebDriver driver =new ChromeDriver();
+		
+	}
+
+}
